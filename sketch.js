c5456b9b9f17d7191f318687b64c3c06324e4a07
aaronttran <101088712+aaronttran@users.noreply.github.com>
2022-03-13 23:49:43 -0700

    Uploading 2.1 p5.js files

---
+++ sketch.js
@@ -0,0 +1,12 @@
+let cars = ["lexus, honda, toyota, mercedes, BMW, audi"]
+
+function setup() {
+  createCanvas(600, 600);
+  background(220);
+  
+  console.log(cars);
+}
+
+function draw() {
+  
+}
